2a077e05620b02d6867dbddc7eea3341f49757b4
trp89 <trp89@pitt.edu>
2024-12-19 13:40:36 -0500

    Reverted tabs to spaces in several files

--- InlineHtmlGalleySidebarBlockPlugin.php
+++ InlineHtmlGalleySidebarBlockPlugin.php
@@ -17,108 +17,108 @@ use APP\plugins\generic\inlineHtmlGalley\InlineHtmlGalleyBlockPlugin;
 
 abstract class InlineHtmlGalleySidebarBlockPlugin extends InlineHtmlGalleyBlockPlugin {
 
-	abstract function blockName();
+    abstract function blockName();
 
-	/**
-	 * Get the display name of this plugin.
-	 * @return String
-	 */
-	function getDisplayName() {
-		return __('plugins.generic.inlineHtmlGalley.block.' . $this->blockName() . '.displayName');
+    /**
+     * Get the display name of this plugin.
+     * @return String
+     */
+    function getDisplayName() {
+        return __('plugins.generic.inlineHtmlGalley.block.' . $this->blockName() . '.displayName');
+    }
+
+    /**
+     * Get a description of the plugin.
+     * @return String
+     */
+    function getDescription() {
+        return __('plugins.generic.inlineHtmlGalley.block.' . $this->blockName() . '.description');
 	}
 
-	/**
-	 * Get a description of the plugin.
-	 * @return String
-	 */
-	function getDescription() {
-		return __('plugins.generic.inlineHtmlGalley.block.' . $this->blockName() . '.description');
-	}
-
-	/**
-	 * Get the name of the block template file.
-	 * @return String
-	 */
-	function getBlockTemplateFilename() {
-		return 'block' . ucfirst($this->blockName()) . '.tpl';
-	}
+    /**
+     * Get the name of the block template file.
+     * @return String
+     */
+    function getBlockTemplateFilename() {
+        return 'block' . ucfirst($this->blockName()) . '.tpl';
+    }
 
 }
 
 class InlineHtmlGalleyAuthorsSidebarBlockPlugin extends InlineHtmlGalleySidebarBlockPlugin {
-	function blockName() {
-		return "authors";
-	}
+    function blockName() {
+        return "authors";
+    }
 }
 
 class InlineHtmlGalleyKeywordsSidebarBlockPlugin extends InlineHtmlGalleySidebarBlockPlugin {
-	function blockName() {
-		return "keywords";
-	}
+    function blockName() {
+        return "keywords";
+    }
 }
 
 class InlineHtmlGalleyDoiSidebarBlockPlugin extends InlineHtmlGalleySidebarBlockPlugin {
-	function blockName() {
-		return "doi";
-	}
+    function blockName() {
+        return "doi";
+    }
 
-	/**
-	 * @copydoc BlockPlugin::getContents()
-	 */
-	function getContents($templateMgr, $request = null) {
-		if ($templateMgr && $request) {
-			$pubIdPlugins = $templateMgr->getTemplateVars('pubIdPlugins');
-			if ($pubIdPlugins) {
-				foreach ($pubIdPlugins as $pubIdPlugin) {
-					if ($pubIdPlugin->getPubIdType() == 'doi') {
-						return parent::getContents($templateMgr, $request);
-					}
-				}
-			}
-		}
+    /**
+     * @copydoc BlockPlugin::getContents()
+     */
+    function getContents($templateMgr, $request = null) {
+        if ($templateMgr && $request) {
+            $pubIdPlugins = $templateMgr->getTemplateVars('pubIdPlugins');
+            if ($pubIdPlugins) {
+                foreach ($pubIdPlugins as $pubIdPlugin) {
+                    if ($pubIdPlugin->getPubIdType() == 'doi') {
+                        return parent::getContents($templateMgr, $request);
+                    }
+                }
+            }
+        }
 
-		return false;
-	}
+        return false;
+    }
 }
 
 class InlineHtmlGalleyCoverImageSidebarBlockPlugin extends InlineHtmlGalleySidebarBlockPlugin {
-	function blockName() {
-		return "coverImage";
-	}
+    function blockName() {
+        return "coverImage";
+    }
 }
 
 class InlineHtmlGalleyDetailsSidebarBlockPlugin extends InlineHtmlGalleySidebarBlockPlugin {
-	function blockName() {
-		return "details";
-	}
+    function blockName() {
+        return "details";
+    }
 }
 
 class InlineHtmlGalleyPublishedDateSidebarBlockPlugin extends InlineHtmlGalleySidebarBlockPlugin {
-	function blockName() {
-		return "publishedDate";
-	}
+    function blockName() {
+        return "publishedDate";
+    }
 }
 
 class InlineHtmlGalleyHowToCiteSidebarBlockPlugin extends InlineHtmlGalleySidebarBlockPlugin {
-	function blockName() {
-		return "howToCite";
-	}
+    function blockName() {
+        return "howToCite";
+    }
 }
 
 class InlineHtmlGalleyLicenseSidebarBlockPlugin extends InlineHtmlGalleySidebarBlockPlugin {
-	function blockName() {
-		return "license";
-	}
+    function blockName() {
+        return "license";
+    }
 }
 
 class InlineHtmlGalleyReferencesSidebarBlockPlugin extends InlineHtmlGalleySidebarBlockPlugin {
-	function blockName() {
-		return "references";
-	}
+    function blockName() {
+        return "references";
+    }
 }
 
 class InlineHtmlGalleyGalleysSidebarBlockPlugin extends InlineHtmlGalleySidebarBlockPlugin {
-	function blockName() {
-		return "galleys";
-	}
+    function blockName() {
+        return "galleys";
+    }
 }
--- templates/blockGalleys.tpl
+++ templates/blockGalleys.tpl
@@ -1,20 +1,20 @@
 
 {if $primaryGalleys || $supplementaryGalleys}
-	<div class="pkp_block block_galleys">
-		<div class="title">
-			<span>{translate key="common.download"}</span>
-		</div>
-		<div class="download">
-			{if $primaryGalleys}
-				{foreach from=$primaryGalleys item=galley}
-					{include file="frontend/objects/galley_link.tpl" parent=$article purchaseFee=$currentJournal->getSetting('purchaseArticleFee') purchaseCurrency=$currentJournal->getSetting('currency')}
-				{/foreach}
-			{/if}
-			{if $supplementaryGalleys}
-				{foreach from=$supplementaryGalleys item=galley}
-					{include file="frontend/objects/galley_link.tpl" parent=$article isSupplementary="1"}
-				{/foreach}
-			{/if}
-		</div>
-	</div>
+    <div class="pkp_block block_galleys">
+        <div class="title">
+            <span>{translate key="common.download"}</span>
+        </div>
+        <div class="download">
+            {if $primaryGalleys}
+                {foreach from=$primaryGalleys item=galley}
+                    {include file="frontend/objects/galley_link.tpl" parent=$article purchaseFee=$currentJournal->getSetting('purchaseArticleFee') purchaseCurrency=$currentJournal->getSetting('currency')}
+                {/foreach}
+            {/if}
+            {if $supplementaryGalleys}
+                {foreach from=$supplementaryGalleys item=galley}
+                    {include file="frontend/objects/galley_link.tpl" parent=$article isSupplementary="1"}
+                {/foreach}
+            {/if}
+        </div>
+    </div>
 {/if}
